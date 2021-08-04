44a5c5b17b8623822f691252b1762aa80e451bd4
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-08-04 20:20:27 +0300

    Create quiz6.py

---
+++ quiz6.py
@@ -0,0 +1,43 @@
+import random
+
+class QA:
+  def __init__(self, question, correctAnswer, otherAnswers):
+    self.question = question
+    self.corrAnsw = correctAnswer
+    self.otherAnsw = otherAnswers
+
+qaList = [QA("H2O is the chemical formula for?", "Water", ["Oxygen", "Iron"]),
+QA("How many Champions League titles has Cristiano Ronaldo win?", "five", ["one", "two", "three"]),
+QA("Which is the first album of heavy metal group Savatage?", "Sirens", ["The Wake of Maggelan", "Hall of the Mountain King", "Piece of Mind"]),
+QA("Which is the first album of heavy metal giants Iron Maiden?", "Iron Maiden", ["Killers", "Brave New World"]),
+QA("Which is the first album of heavy metal giants Metallica?", "Kill'em all", ["Load", "Black album", "Ride the lightning", "St.Anger"])]
+
+corrCount = 0
+random.shuffle(qaList)
+for qaItem in qaList:
+  print(qaItem.question)
+  print("Possible answers are:")
+  possible = qaItem.otherAnsw + [qaItem.corrAnsw] # square brackets turn correct answer into list for concatenating with other list
+  random.shuffle(possible)
+  count = 0 # list indexes start at 0 in python
+  while count < len(possible):
+    print(str(count+1) + ": " + possible[count])
+    count += 1
+  print("Please enter the number of your answer:")
+  userAnsw = input()
+  while not userAnsw.isdigit():
+    print("That was not a number. Please enter the number of your answer:")
+    userAnsw = input()
+  userAnsw = int(userAnsw)
+  while not (userAnsw > 0 and userAnsw <= len(possible)):
+    print("That number doesn't correspond to any answer. Please enter the number of your answer:")
+    userAnsw = input()
+  if possible[userAnsw-1] == qaItem.corrAnsw:
+    print("Your answer was correct.")
+    corrCount += 1
+  else:
+    print("Your answer was wrong.")
+    print("Correct answer was: " + qaItem.corrAnsw)
+  print("")
+
+print("You answered " + str(corrCount) + " of " + str(len(qaList)) + " questions correctly.")
